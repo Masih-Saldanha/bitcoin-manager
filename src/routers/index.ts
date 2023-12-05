c7db350dc00607582c3e685988d64b14789d6c92
Masih Saldanha Xavier <masih.saldanha@gmail.com>
2023-12-04 22:14:12 -0300

    feat: add get health route

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
+import healthRouter from "./healthRouter.js";
 import bitcoinRouter from "./bitcoinRouter.js";
 
 const router = Router();
 
+router.use(healthRouter);
 router.use(bitcoinRouter);
 
 export default router;
